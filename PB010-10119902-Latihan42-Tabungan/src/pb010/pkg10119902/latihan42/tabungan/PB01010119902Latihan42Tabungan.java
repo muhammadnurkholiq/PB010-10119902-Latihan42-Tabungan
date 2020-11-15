ecc09e918ec06b6d06945013d104c864cceb52c7
muhammadnurkholiq <73203850+muhammadnurkholiq@users.noreply.github.com>
2020-11-15 09:05:29 +0700

    Add files via upload

---
+++ PB010-10119902-Latihan42-Tabungan/src/pb010/pkg10119902/latihan42/tabungan/PB01010119902Latihan42Tabungan.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pb010.pkg10119902.latihan42.tabungan;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 
+ * NAMA     : Muhammad Nurkholiq
+ * KELAS    : IF10K
+ * NIM      : 10119902
+ * Deskripsi Program    : Program Ini Berisi Program Untuk Menampilkan Tabungan
+ * 
+ */
+
+public class PB01010119902Latihan42Tabungan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Scanner input = new Scanner(System.in);
+
+        System.out.println("==== Program Penarikan Uang ====");
+        System.out.print("Masukan saldo Awal : ");
+        Tabungan tbg = new Tabungan(input.nextInt());
+        System.out.print("Jumlah uang yang diambil : ");
+        int jumlah = input.nextInt();
+        System.out.println("Saldo anda sekarang : "+tbg.ambilUang(jumlah));
+    }
+    
+}
---
+++ PB010-10119902-Latihan42-Tabungan/src/pb010/pkg10119902/latihan42/tabungan/Tabungan.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pb010.pkg10119902.latihan42.tabungan;
+
+/**
+ *
+ * @author korik
+ */
+public class Tabungan {
+    private final int saldo;
+    
+    public Tabungan (int saldo){
+        this.saldo = saldo;
+        
+    }
+    public int ambilUang(int jumlah){
+        return saldo - jumlah;
+    }
+}
